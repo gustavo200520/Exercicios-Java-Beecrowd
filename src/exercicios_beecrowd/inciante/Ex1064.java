118e696ded94e4a3f69775056d708d80fe3db407
Gustavo Matos Barbosa <gustavomatosbarbosa2@gmail.com>
2026-03-14 20:23:23 -0300

    Adicao Ex1064

--- src/exercicios_beecrowd/inciante/Ex1064.java
+++ src/exercicios_beecrowd/inciante/Ex1064.java
@@ -2,28 +2,27 @@ package exercicios_beecrowd.inciante;
 
 import java.io.IOException;
 import java.util.Scanner;
-//Lembrar que no beecrowd a classe deve ser Main e nao pode ter o package
-public class Ex1060 {
-
-    public static void main(String[] args) throws IOException {
+//Lembrar que no beecrowd a classe deve ser Main nao pode ter o package
+public class Ex1064 {
+	public static void main(String[] args) throws IOException {
         Scanner teclado = new Scanner(System.in);
-        int tamanho=6;
-        int contador_numeros = 0;
+        int tamanho=6, contador_numeros = 0;
+        float media = 0;
         float [] numero = new float[tamanho];
+        
         for(int cont=0;cont<tamanho; cont++) {
-
             numero[cont] = teclado.nextFloat();
         }
 
-
         for(int cont=0;cont<tamanho;cont++) {
-
             if(numero[cont]>0) {
                 contador_numeros++;
+                media = (media + numero[cont]);
             }
-
         }
+        
         System.out.printf("%d valores positivos\n",contador_numeros);
-
+        System.out.printf("%.1f\n",media/contador_numeros);
     }
+
 }
